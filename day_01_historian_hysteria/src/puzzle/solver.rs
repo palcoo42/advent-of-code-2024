c0c9be7b8ef6ed440161cb0356f277ec47ff9dc7
Pavol Ondrušek <pavol.ondrusek@gmail.com>
2024-12-18 12:21:06 +0100

    Add unit tests for verification of solution part_1 and part_2 for all puzzles

--- day_01_historian_hysteria/src/puzzle/solver.rs
+++ day_01_historian_hysteria/src/puzzle/solver.rs
@@ -33,3 +33,79 @@ impl PuzzleSolver for Solver {
         Ok(self.locations.get_similarity_score().to_string())
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use std::{
+        fs::File,
+        io::{BufRead, BufReader},
+        sync::{LazyLock, Mutex},
+    };
+
+    use advent_of_code::env::project::Project;
+
+    use super::*;
+
+    fn create_solver() -> &'static Mutex<Solver> {
+        static SOLVER: LazyLock<Mutex<Solver>> = LazyLock::new(|| {
+            // Read input file
+            let input_file = Project::new().resource_file("input.txt");
+            let file = File::open(&input_file)
+                .unwrap_or_else(|err| panic!("Failed to open file with an error '{}'", err));
+            let reader = BufReader::new(file);
+            let lines = reader
+                .lines()
+                .collect::<Result<Vec<String>, _>>()
+                .unwrap_or_else(|err| panic!("Failed to unwrap lines with an error '{}'", err));
+
+            let lines: Vec<&str> = lines.iter().map(|s| s.as_str()).collect();
+
+            // Prepare solver
+            let mut solver = Solver::new();
+
+            // Parse input file
+            solver
+                .parse_input_file(&lines)
+                .unwrap_or_else(|err| panic!("Failed to parse input file with error '{}'", err));
+
+            Mutex::new(solver)
+        });
+
+        &SOLVER
+    }
+
+    #[test]
+    fn test_part_1() {
+        let result;
+
+        // Solve the puzzle inside a scope so that guard is released automatically avoiding a panic in the thread.
+        {
+            let solver = create_solver()
+                .lock()
+                .expect("Failed to unwrap Solver Mutex");
+
+            result = solver.part_1();
+        }
+
+        assert!(result.is_ok(), "Result: {:?}", result);
+        assert_eq!(result.unwrap(), String::from("2378066"));
+    }
+
+    #[test]
+    fn test_part_2() {
+        let result;
+
+        // Solve the puzzle inside a scope so that guard is released automatically avoiding a panic in the thread.
+        {
+            let solver = create_solver()
+                .lock()
+                .expect("Failed to unwrap Solver Mutex");
+
+            result = solver.part_2();
+        }
+
+        assert!(result.is_ok(), "Result: {:?}", result);
+        assert_eq!(result.unwrap(), String::from("18934359"));
+    }
+}
--- day_03_mull_it_over/src/puzzle/solver.rs
+++ day_03_mull_it_over/src/puzzle/solver.rs
@@ -65,40 +65,76 @@ impl PuzzleSolver for Solver {
 
 #[cfg(test)]
 mod tests {
+
+    use std::{
+        fs::File,
+        io::{BufRead, BufReader},
+        sync::{LazyLock, Mutex},
+    };
+
+    use advent_of_code::env::project::Project;
+
     use super::*;
 
-    fn create_solver() -> Solver {
-        let mut solver = Solver::new();
+    fn create_solver() -> &'static Mutex<Solver> {
+        static SOLVER: LazyLock<Mutex<Solver>> = LazyLock::new(|| {
+            // Read input file
+            let input_file = Project::new().resource_file("input.txt");
+            let file = File::open(&input_file)
+                .unwrap_or_else(|err| panic!("Failed to open file with an error '{}'", err));
+            let reader = BufReader::new(file);
+            let lines = reader
+                .lines()
+                .collect::<Result<Vec<String>, _>>()
+                .unwrap_or_else(|err| panic!("Failed to unwrap lines with an error '{}'", err));
 
-        solver.instructions = vec![
-            Instruction::Multiply(2, 4),
-            Instruction::DoNot,
-            Instruction::Multiply(5, 5),
-            Instruction::Multiply(11, 8),
-            Instruction::Do,
-            Instruction::Multiply(8, 5),
-        ];
+            let lines: Vec<&str> = lines.iter().map(|s| s.as_str()).collect();
 
-        solver
+            // Prepare solver
+            let mut solver = Solver::new();
+
+            // Parse input file
+            solver
+                .parse_input_file(&lines)
+                .unwrap_or_else(|err| panic!("Failed to parse input file with error '{}'", err));
+
+            Mutex::new(solver)
+        });
+
+        &SOLVER
     }
 
     #[test]
     fn test_part_1() {
-        let solver = create_solver();
+        let result;
 
-        let result = solver.part_1();
+        // Solve the puzzle inside a scope so that guard is released automatically avoiding a panic in the thread.
+        {
+            let solver = create_solver()
+                .lock()
+                .expect("Failed to unwrap Solver Mutex");
+
+            result = solver.part_1();
+        }
 
         assert!(result.is_ok(), "Result: {:?}", result);
-        assert_eq!(solver.part_1().unwrap(), "161");
+        assert_eq!(result.unwrap(), String::from("187194524"));
     }
 
     #[test]
     fn test_part_2() {
-        let solver = create_solver();
+        let result;
 
-        let result = solver.part_2();
+        // Solve the puzzle inside a scope so that guard is released automatically avoiding a panic in the thread.
+        {
+            let solver = create_solver()
+                .lock()
+                .expect("Failed to unwrap Solver Mutex");
+
+            result = solver.part_2();
+        }
 
         assert!(result.is_ok(), "Result: {:?}", result);
-        assert_eq!(solver.part_2().unwrap(), "48");
+        assert_eq!(result.unwrap(), String::from("127092535"));
     }
 }
